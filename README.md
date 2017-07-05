c4fdd85cdf30078cd3bd38c51d1b8610af758c05
Chris Henderson <chris@winklevosscapital.com>
2017-07-05 00:56:11 -0700

    initial working commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # webpack-sass-barebones
+
+Basic working example of webpack / sass / autoprefixer thing.
+
+1. Install node/npm if you don't already have it:
+
+    ```
+    $ brew install node
+    ```
+    
+2. Install npm packages:
+    ```
+    $ npm install
+    ```
+3. Run that shit
+    1. To run dev server on [http://localhost:8080](http://localhost:8080) with hot module reloading (the CSS is inserted into the page dynamically and can be viewed in the "Elements" tab of Chrome):
+        ```
+        $ npm run dev
+        ```
+    1. To build the CSS file as a standalone thing in `dist/main.css`:
+        ```
+        $ npm run build    
+        ```
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const pkg = require('./package.json');
+const dirs = pkg['h5bp-configs'].directories;
+
+const isProd = process.env.NODE_ENV === 'production';
+const webpackPlugins = [];
+if (isProd) {
+  webpackPlugins.push(new ExtractTextPlugin({
+    filename: "main.css"
+  }))
+} else {
+  webpackPlugins.push(new webpack.HotModuleReplacementPlugin())
+}
+webpackPlugins.push(new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, dirs.src.web, 'index.html'),
+  inject: 'body'
+}))
+module.exports = {
+  entry: path.resolve(dirs.src.web, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, dirs.dist),
+    pathinfo: true,
+    publicPath: '/',
+    filename: 'main.js'
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, dirs.dist),
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/, enforce: 'pre', use: [{
+        loader: "sass-loader", options: {
+          outputStyle: 'expanded'
+        }
+      }]
+    }, {
+        test: /\.scss$/,
+        use: isProd ? ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'autoprefixer-loader']
+        }) : ['css-loader', 'autoprefixer-loader']
+    }]
+  },
+  plugins: webpackPlugins
+}
